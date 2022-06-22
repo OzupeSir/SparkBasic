a71c5d71c84a0f5cc94a5e6903e6316b8f605542
张津培 <zhang8262298@163.com>
2022-06-22 23:53:35 +0800

    2022-06-22 home commit

---
+++ spark-core/src/main/java/com/ozupesir/bigdata/spark/core/wc/Spark03_WordCount.scala
@@ -0,0 +1,41 @@
+package com.ozupesir.bigdata.spark.core.wc
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark03_WordCount {
+  def main(args: Array[String]): Unit = {
+    //Application
+
+    // Spark框架
+    // Todo 建立和Spark框架的连接
+    // JDBC : connetcion
+
+    val sparkConf = new SparkConf().setMaster("local").setAppName("WorlCount")
+    val sc = new SparkContext(sparkConf)
+
+    // Todo 执行业务作业
+    // 1. 读取文件，获取一行一行的数据
+    // hello world
+    val lines: RDD[String] = sc.textFile(path = "datas")
+
+    // 2. 将一行数据进行拆分，形成一个一个的单词（分词）
+    // 扁平化：将整体拆分成个体的操作
+    // "hello world"  => hello,world,hello,world
+    val words: RDD[String] = lines.flatMap(_.split(" "))
+
+    val wordToOne = words.map((_, 1))
+
+    // Spark框架提供了更多的功能，可以将分组和聚合使用一个方法来实现
+    val wordToCount: RDD[(String, Int)] = wordToOne.reduceByKey(_ + _)
+
+
+    val array: Array[(String, Int)] = wordToCount.collect()
+    array.foreach(println)
+    //  println(array)
+
+    // Todo 关闭连接
+    sc.stop()
+
+  }
+}
