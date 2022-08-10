e7948690b76de8ed858985d45b755a2411416d4a
张津培 <zhang8262298@163.com>
2022-08-10 12:45:19 +0800

    2022-08-10 thinkpad commit

---
+++ spark-core/src/main/java/com/ozupesir/bigdata/spark/core/rdd/operator/transform/Spark04_RDD_Operator_Transform2.scala
@@ -0,0 +1,29 @@
+package com.ozupesir.bigdata.spark.core.rdd.operator.transform
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark04_RDD_Operator_Transform2 {
+
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("RDD")
+    val sc = new SparkContext(sparkConf)
+
+    // TODO 算子 - mapPartitions
+    val rdd: RDD[Any] = sc.makeRDD(List(List(1, 2), 3, List(4, 5)))
+
+    val flatRDD: RDD[Any] = rdd.flatMap(
+      data => {
+        data match {
+          case list: List[_] => list
+          case dat => List(dat)
+        }
+      }
+    )
+
+    flatRDD.collect().foreach(println)
+
+
+    sc.stop()
+  }
+}
---
+++ spark-core/src/main/java/com/ozupesir/bigdata/spark/core/rdd/operator/transform/Spark06_RDD_Operator_Transform1.scala
@@ -0,0 +1,24 @@
+package com.ozupesir.bigdata.spark.core.rdd.operator.transform
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark06_RDD_Operator_Transform1 {
+
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("RDD")
+    val sc = new SparkContext(sparkConf)
+
+    // TODO 算子 - groupBy
+    val rdd: RDD[String] = sc.makeRDD(List("Hello", "Spark", "Scala", "Hadoop"), 2)
+
+    // groupBy会将数据源中的每一个数据进行分组判断，根据返回的分组key进行分组
+    // 相同的key值的数据会放置在一个组中
+
+    val groupRDD = rdd.groupBy(_.charAt(0))
+
+    groupRDD.collect().foreach(println)
+
+    sc.stop()
+  }
+}
