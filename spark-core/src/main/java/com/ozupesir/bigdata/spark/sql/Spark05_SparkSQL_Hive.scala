df9c74295959276b35ca3e754ef59a9092f3395f
张津培 <zhang8262298@163.com>
2022-11-22 01:32:30 +0800

    2022-11-21 home commit

--- spark-core/src/main/java/com/ozupesir/bigdata/spark/sql/Spark05_SparkSQL_Hive.scala
+++ spark-core/src/main/java/com/ozupesir/bigdata/spark/sql/Spark05_SparkSQL_Hive.scala
@@ -7,13 +7,14 @@ object Spark05_SparkSQL_Hive {
   def main(args: Array[String]): Unit = {
     // TODO 创建SparkSQL的运行环境
     val sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkSQL")
-    val spark: SparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+    val spark: SparkSession = SparkSession.builder().config(sparkConf).enableHiveSupport().getOrCreate()
 
     // 使用SparkSQL连接外置的Hive
-    // 1. 拷贝Hive-size.xml文件到classpath下
+    // 1. 拷贝Hive-site.xml文件到classpath下
     // 2. 启用Hive的支持
-    // 3. 增加对应的以来关系（包含MySQL驱动）
-    spark.sql("show tables").show
+    // 3. 增加对应的依赖关系（包含Mysql驱动）
+   
+   spark.sql("show databases").show
 
     // TODO 关闭环境
     spark.close()
---
+++ spark-core/src/main/java/com/ozupesir/bigdata/spark/sql/Spark05_SparkSQL_test2.scala
@@ -0,0 +1,91 @@
+package com.ozupesir.bigdata.spark.sql
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql._
+
+object Spark05_SparkSQL_test2 {
+	def main(args: Array[String]): Unit = {
+		// TODO 创建SparkSQL的运行环境
+		System.setProperty("HADDOOP_USER_NAME", "root")
+		
+		val sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkSQL")
+		val spark: SparkSession = SparkSession.builder().enableHiveSupport().config(sparkConf).getOrCreate()
+		
+		// 使用SparkSQL连接外置的Hive
+		// 1. 拷贝Hive-site.xml文件到classpath下
+		// 2. 启用Hive的支持
+		// 3. 增加对应的依赖关系（包含Mysql驱动）
+		
+//		spark.sql("use ozupesir").show()
+		
+		spark.sql("use ozupesir")
+		
+		spark.sql("drop table if exists user_visit_action")
+		spark.sql("drop table if exists product_info")
+		spark.sql("drop table if exists city_info")
+		
+		spark.sql(
+			"""
+			  | CREATE TABLE `user_visit_action`(
+			  |  `date` string,
+			  |  `user_id` bigint,
+			  |  `session_id` string,
+			  |  `page_id` bigint,
+			  |  `action_time` string,
+			  |  `search_keyword` string,
+			  |  `click_category_id` bigint,
+			  |  `click_product_id` bigint,
+			  |  `order_category_ids` string,
+			  |  `order_product_ids` string,
+			  |  `pay_category_ids` string,
+			  |  `pay_product_ids` string,
+			  |  `city_id` bigint)
+			  | row format delimited fields terminated by '\t'
+			  |""".stripMargin)
+		
+		spark.sql(
+			"""
+			  | load data local inpath 'sparkSqlDatas/user_visit_action.txt' into table user_visit_action
+			  |""".stripMargin)
+		
+		spark.sql(
+			"""
+			  | CREATE TABLE `product_info`(
+			  |    `product_id` bigint,
+			  |    `product_name` string,
+			  |    `extend_info` string)
+			  |    row format delimited fields terminated by '\t'
+			  |""".stripMargin)
+		
+		spark.sql(
+			"""
+			  | load data local inpath 'sparkSqlDatas/product_info.txt' into table product_info
+			  |""".stripMargin)
+		
+		spark.sql(
+			"""
+			  | CREATE TABLE `city_info`(
+			  |  `city_id` bigint,
+			  |  `city_name` string,
+			  |  `area` string)
+			  |  row format delimited fields terminated by '\t'
+			  |""".stripMargin)
+		
+		spark.sql(
+			"""
+			  | load data local inpath 'sparkSqlDatas/city_info.txt' into table city_info
+			  |""".stripMargin)
+		
+//		spark.sql(
+//			"""
+//			  | select count(1) from user_visit_action
+//			  | union
+//			  | select count(1) from product_info
+//			  | union
+//			  | select count(1) from city_info
+//			  | """.stripMargin)
+		
+		// TODO 关闭环境
+		spark.close()
+	}
+}
